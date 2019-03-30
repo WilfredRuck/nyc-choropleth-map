971f287b4443f9bfe5c73075348937dc8acb8935
Wilfred Ruck <wilfredoruck@gmail.com>
2019-03-30 13:57:09 -0400

    move map data to seperate file

--- src/nyc.js
+++ src/nyc.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import ReactNYC from 'react-nyc-choropleth';
+import { cityData } from './nycData.js';
 
 class NYC extends React.Component {
   render() {
     const mapboxAccessToken = "pk.eyJ1Ijoid2lsbHRoZWNvZGVyIiwiYSI6ImNqdHZxcmNqbjIwcnMzem11Zm1kNWR6cmUifQ.5u0xI9_WLYtZZSfCKosTZg" // Your access token
     const mapboxType = "streets";
     const position = [40.7831, -73.9712];
-    const zoom = 12;
-    const data = [
-      {
-        name: "Chelsea",
-        values: [{label: "Population", val: 20000}, {label: "Restaurants", val: "690"}],
-        color: "#E31A1C"
-      }
-    ]
+    const zoom = 11.5;
+    const data = cityData;
     const neighborhoodStyle = { weight: 1, opacity: 1, color: '#666', dashArray: '3', fillOpacity: 0.7 };
     const neighborhoodHoverStyle = { weight: 5, color: '#FFF', dashArray: '1', fillOpacity: 0.7 };
-    const excludeNeighborhoods = ["Liberty Island", "Ellis Island"];
+    const excludeNeighborhoods = ["Liberty Island", "Ellis Island", "Governors Island"];
 
     return (
       <div>
---
+++ src/nycData.js
@@ -0,0 +1,12 @@
+export const cityData = [
+  {
+    name: "Chelsea",
+    values: [{label: "Population", val: 20000}],
+    color: "#E31A1C"
+  },
+  {
+    name: "Central Park",
+    values: [{label: "Population", val: 0}],
+    color: "#F35G1D"
+  }
+] 
