43843ee81e4e09d711d2eb83ba4b54c968c721f5
Wilfred Ruck <wilfredoruck@gmail.com>
2019-03-31 19:11:47 -0400

    add more data

--- src/nycData.js
+++ src/nycData.js
@@ -7,6 +7,21 @@ export const cityData = [
   {
     name: "Central Park",
     values: [{label: "Population", val: 0}],
-    color: "#F35G1D"
+    color: "#f04cb9"
+  },
+  {
+    name: "Upper East Side",
+    values: [{label: "Population", val: 5000}],
+    color: "#F35S5T"
+  },
+  {
+    name: "Washington Heights",
+    values: [{label: "Population", val: 3000}],
+    color: "#cf7ef2"
+  },
+  {
+    name: "Lower East Side",
+    values: [{label: "Population", val: 3500}],
+    color: "#8f4fcf"
   }
 ] 
