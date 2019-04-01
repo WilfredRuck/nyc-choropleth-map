a345796210ae3a679c46b5913158fbe521a495e2
Wilfred Ruck <wilfredoruck@gmail.com>
2019-04-01 15:55:10 -0400

    add all zipcodes and set colors

--- src/nycData.js
+++ src/nycData.js
@@ -23,5 +23,150 @@ export const cityData = [
     name: "Lower East Side",
     values: [{label: "Population", val: 3500}],
     color: "#8f4fcf"
+  },
+  {
+    name: "Spanish Harlem",
+    values: [{label: "Population", val: 500}],
+    color: "#e1f964"
+  },
+  {
+    name: "Inwood",
+    values: [{label: "Population", val: 2000}],
+    color: "#2091c3"
+  },
+  {
+    name: "Marble Hill",
+    values: [{label: "Population", val: 500}],
+    color: "#f570f2"
+  },
+  {
+    name: "Hamilton Heights",
+    values: [{label: "Population", val: 900}],
+    color: "#173b84"
+  },
+  {
+    name: "Morningside Heights",
+    values: [{label: "Population", val: 750}],
+    color: "#490d08"
+  },
+  {
+    name: "Upper West Side",
+    values: [{label: "Population", val: 4300}],
+    color: "#992266"
+  },
+  {
+    name: "Midtown West",
+    values: [{label: "Population", val: 2250}],
+    color: "#a83495"
+  },
+  {
+    name: "Hell's Kitchen",
+    values: [{label: "Population", val: 600}],
+    color: "#04c140"
+  },
+  {
+    name: "Midtown East",
+    values: [{label: "Population", val: 720}],
+    color: "#ef3055"
+  },
+  {
+    name: "Murray Hill",
+    values: [{label: "Population", val: 300}],
+    color: "#fa8b90"
+  },
+  {
+    name: "Kips Bay",
+    values: [{label: "Population", val: 230}],
+    color: "#448d67"
+  },
+  {
+    name: "Flatiron District",
+    values: [{label: "Population", val: 500}],
+    color: "#c95da9"
+  },
+  {
+    name: "Gramercy",
+    values: [{label: "Population", val: 500}],
+    color: "#3b7965"
+  },
+  {
+    name: "Stuyvesant Town",
+    values: [{label: "Population", val: 3500}],
+    color: "#fd3744"
+  },
+  {
+    name: "East Village",
+    values: [{label: "Population", val: 2400}],
+    color: "#159aad"
+  },
+  {
+    name: "NoHo",
+    values: [{label: "Population", val: 150}],
+    color: "#a391f6"
+  },
+  {
+    name: "Greenwich Village",
+    values: [{label: "Population", val: 1100}],
+    color: "#da5391"
+  },
+  {
+    name: "West Village",
+    values: [{label: "Population", val: 2100}],
+    color: "#ed9f5e"
+  },
+  {
+    name: "SoHo",
+    values: [{label: "Population", val: 2000}],
+    color: "#adce95"
+  },
+  {
+    name: "Nolita",
+    values: [{label: "Population", val: 100}],
+    color: "#df5351"
+  },
+  {
+    name: "Tribeca",
+    values: [{label: "Population", val: 3200}],
+    color: "#f0f1cd"
+  },
+  {
+    name: "City Hall Area",
+    values: [{label: "Population", val: 50}],
+    color: "#82c59b"
+  },
+  {
+    name: "Chinatown",
+    values: [{label: "Population", val: 950}],
+    color: "#4351cb"
+  },
+  {
+    name: "Little Italy",
+    values: [{label: "Population", val: 200}],
+    color: "#03f6eb"
+  },
+  {
+    name: "Battery Park City",
+    values: [{label: "Population", val: 1700}],
+    color: "#e6720b"
+  },
+  {
+    name: "Financial District",
+    values: [{label: "Population", val: 1200}],
+    color: "#ebb4bf"
+  },
+  {
+    name: "Two Bridges",
+    values: [{label: "Population", val: 200}],
+    color: "#5c70a8"
+  },
+  {
+    name: "Roosevelt Island",
+    values: [{label: "Population", val: 2000}],
+    color: "#806645"
+  },
+  {
+    name: "Randall's Island",
+    values: [{label: "Population", val: 500}],
+    color: "#d09b0e"
   }
 ] 
